f306b93bd65ecd8eaad0d0bf6a65be4b0b6ed6e3
Nikhil Shridhar <nikhil95@gmail.com>
2017-05-09 11:57:01 -0700

    fixed orientation issue

--- app/src/main/java/com/example/nikhil/offinaheartbeat/MainActivity.java
+++ app/src/main/java/com/example/nikhil/offinaheartbeat/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
